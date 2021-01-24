746228ac2d5423ee13ae5bd9f7341d580ce1f4c1
alefranco41 <42209092+alefranco41@users.noreply.github.com>
2021-01-24 12:49:55 +0100

    Add files via upload

---
+++ PROJECTWORK/js/BMR.js
@@ -0,0 +1,229 @@
+var metabolismoBasale = 0;
+var spanArray = document.getElementsByTagName('span');
+var inputArray = document.getElementsByTagName('input');
+var allenamento = document.getElementsByName('Allenamento');
+var cardio = document.getElementsByName('Cardio');
+var pesi = document.getElementsByName('Pesi');
+var sesso = "";
+var f = "";
+var arrayMoltiplicatori = [1.2,1.235,1.27,1.305,1.34,1.375,1.41,1.445,1.48,1.515,1.55,1.585,1.62,1.655,1.69,1.725,1.76,1.795,1.83,1.9];
+
+function bmr() {
+
+  var radios = document.getElementsByName('Sesso');
+  for (var i=0; i<radios.length; i++) {
+    if (radios[i].checked) {
+      sesso = radios[i].value;
+    }
+  }
+  var peso = document.getElementById('Peso').value;
+  var altezza = document.getElementById('Altezza').value;
+  var eta = document.getElementById('Età').value;
+  var massaGrassa = document.getElementById('MassaGrassa').value;
+  if(f == "hb"){
+    metabolismoBasale = hb(sesso, peso, altezza, eta);
+  }else if(f == "rhb"){
+    metabolismoBasale = rhb(sesso, peso, altezza, eta);
+  }else if(f == "msj"){
+    metabolismoBasale = msj(sesso, peso, altezza, eta);
+  }else if(f == "km"){
+    metabolismoBasale = km(peso, massaGrassa);
+  }else if(f == "s"){
+    metabolismoBasale = s(sesso, peso, eta);
+  }
+
+
+ calcoloTDEE(metabolismoBasale);
+
+}
+
+
+function controlloFormula(formula) {
+  f = formula;
+  valoriDefault();
+  if(formula == "km"){
+    nascondi([0,2,3]);
+    rimuoviDisabled([2,5,6,7,15]);
+    aggiungiDisabled([0,1,3,4,8,9,10,11,12,13]);
+  }else if(formula == "s"){
+    nascondi([2,4]);
+    rimuoviDisabled([0,1,2,4,6,7,15]);
+    aggiungiDisabled([3,5,8,9,10,11,12,13]);
+  }else if(formula == "hb" || formula == "rhb" || formula == "msj"){
+    nascondi([4]);
+    rimuoviDisabled([0,1,2,3,4,6,7,15]);
+    aggiungiDisabled([5,8,9,10,11,12,13]);
+  }
+}
+
+function controlloAllenamento() {
+  if(allenamento[0].checked){
+    visualizza([6,8]);
+    rimuoviDisabled([8,9,11,12]);
+  }else{
+    nascondi([6,7,8,9]);
+    aggiungiDisabled([8,9,11,12]);
+    cardio[1].checked = "true";
+    pesi[1].checked = "true";
+  }
+}
+
+function controlloCardio() {
+  if(cardio[0].checked){
+    visualizza([7]);
+    rimuoviDisabled([10]);
+  }else{
+    nascondi([7]);
+    aggiungiDisabled([10]);
+  }
+}
+
+function controlloPesi() {
+  if(pesi[0].checked){
+    visualizza([9]);
+    rimuoviDisabled([13]);
+  }else{
+    nascondi([9]);
+    aggiungiDisabled([13]);
+  }
+}
+
+function valoriDefault() {
+  for(var i=0; i<spanArray.length; i++){
+    if(i<6) {
+      visualizza([i]);
+    }
+  }
+}
+
+function hb(sesso, peso, altezza, eta) {
+  if(sesso == "m"){
+    return (66.5+(13.75*peso)+(5.003*altezza)-(6.755*eta));
+  }else if(sesso == "f"){
+    return (655+(9.563*peso)+(1.850*altezza)-(4.676*eta));
+  }
+}
+
+function rhb(sesso, peso, altezza, eta) {
+  if(sesso == "m"){
+    return (88.362+(13.397*peso)+(4.799*altezza)-(5.677*eta));
+  }else if(sesso == "f"){
+    return (447.593+(9.247*peso)+(3.098*altezza)-(4.7*eta));
+  }
+}
+
+function msj(sesso, peso, altezza, eta) {
+  if(sesso == "m"){
+    return (5+(10*peso)+(6.25*altezza)-(5*eta));
+  }else if(sesso == "f"){
+    return (-161+(10*peso)+(6.25*altezza)-(5*eta));
+  }
+}
+
+function km(peso, massaGrassa) {
+ return (370+(21.6*(peso*(1-(massaGrassa/100)))));
+}
+
+function s(sesso, peso, eta) {
+  if(sesso == "m"){
+    if(eta<3){
+      return ((59.512 * peso) -30.4);
+    }else if(eta>=3 && eta<10){
+      return ((22.706 * peso) +504.3);
+    }else if(eta>=10 && eta<18){
+      return ((17.686 * peso) +658.2);
+    }else if(eta>=18 && eta<30){
+      return ((15.057 * peso) +692.2);
+    }else if(eta>=30 && eta<60){
+      return ((11.472 * peso) +873.1);
+    }else if(eta>=60){
+      return ((11.711 * peso) +587.7);
+    }
+  }else if(sesso == "f"){
+    if(eta<3){
+      return ((58.317 * peso) -31.1);
+    }else if(eta>=3 && eta<10){
+      return ((20.315 * peso) +485.9);
+    }else if(eta>=10 && eta<18){
+      return ((13.384 * peso) +692.6);
+    }else if(eta>=18 && eta<30){
+      return ((14.818 * peso) +486.6);
+    }else if(eta>=30 && eta<60){
+      return ((8.126 * peso) +845.6);
+    }else if(eta>=60){
+      return ((9.082 * peso) +658.5);
+    }
+  }
+}
+
+function visualizza(a){
+  for (var i=0; i<a.length; i++) {
+    spanArray[a[i]].style.display = "inline";
+  }
+}
+
+function nascondi(b){
+  for (var i=0; i<b.length; i++) {
+    spanArray[b[i]].style.display = "none";
+  }
+}
+
+function rimuoviDisabled(c){
+  for (var i=0; i<c.length; i++) {
+    inputArray[c[i]].removeAttribute('disabled');
+  }
+  document.getElementById('lavoro').removeAttribute('disabled');
+}
+
+function aggiungiDisabled(d){
+  for (var i=0; i<d.length; i++) {
+    inputArray[d[i]].setAttribute('disabled', 'disabled');
+  }
+}
+
+function calcoloTDEE(metabolismoBasale) {
+  var livelloSportivo = null;
+  var somma = calcoloGiorniCardio() + calcoloGiorniPesi();
+  var attivita = document.getElementById('lavoro').value;
+
+  if(somma < 2){
+    livelloSportivo = 1;
+  }else if(somma < 4){
+    livelloSportivo = 2;
+  }
+  else if(somma < 6){
+    livelloSportivo = 3;
+  }
+  else if(somma <= 7){
+    livelloSportivo = 4;
+  }
+  else if(somma > 7){
+    livelloSportivo = 5;
+  }
+
+  var indexMoltiplicatore = ((livelloSportivo * attivita)-1);
+  var moltiplicatore = arrayMoltiplicatori[indexMoltiplicatore];
+  var TDEE = metabolismoBasale * moltiplicatore;
+
+  document.getElementById('prova').value = TDEE;
+}
+
+function calcoloGiorniCardio() {
+  GiorniCardio = document.getElementById('GiorniCardio').value;
+  if(isNaN(parseInt(GiorniCardio))){
+    GiorniCardio = 0;
+  }else{
+    GiorniCardio = parseInt(GiorniCardio);
+  }
+  return  GiorniCardio;
+}
+
+function calcoloGiorniPesi() {
+  GiorniPesi = document.getElementById('GiorniPesi').value;
+  if(isNaN(parseInt(GiorniPesi))){
+    GiorniPesi = 0;
+  }else{
+    GiorniPesi = parseInt(GiorniPesi);
+  }
+  return GiorniPesi;
+}
---
+++ PROJECTWORK/pagine/tdee.php
@@ -0,0 +1,103 @@
+<?php
+  print_r($_POST["prova"]);
+  if (array_key_exists("invia", $_POST)) {
+    if (!array_key_exists('Sesso', $_POST) && ($_POST["formule"] != "km")) {
+      $errori .= "Errore sesso <br>";
+    }
+    if(!is_numeric($_POST["Peso"]) || (($_POST["Peso"]<1) || ($_POST["Peso"]>635))){
+      $errori .= "Errore peso <br>";
+    }
+    if((($_POST["formule"] != "km") && ($_POST["formule"] != "s")) && (!is_numeric($_POST["Altezza"]) || $_POST["Altezza"] < 50 || $_POST["Altezza"] > 251)){
+      $errori .= "Errore altezza <br>";
+    }
+    if(($_POST["formule"] != "km") && (!is_numeric($_POST["Eta"]) || $_POST["Eta"] < 0 || $_POST["Eta"] > 118)){
+      $errori .= "Errore Età <br>";
+    }
+    if(($_POST["formule"] == "km") && (!is_numeric($_POST["MassaGrassa"]) || $_POST["MassaGrassa"] < 2 || $_POST["MassaGrassa"] > 80)){
+      $errori .= "Errore Massa Grassa <br>";
+    }
+    if (!array_key_exists('Allenamento', $_POST)) {
+      $errori .= "Errore Allenamento <br>";
+    }
+
+    if (!array_key_exists('Cardio', $_POST) && array_key_exists('Allenamento', $_POST) && $_POST["Allenamento"] == "si"){
+        $errori .= "Errore Cardio <br>";
+    }
+
+    if (array_key_exists('Cardio', $_POST) && array_key_exists('Allenamento', $_POST) && $_POST["Cardio"] == "si" && $_POST["Allenamento"] == "si"){
+      if(!is_numeric($_POST["GiorniCardio"]) || $_POST["GiorniCardio"] < 0 || $_POST["GiorniCardio"] > 7){
+        $errori .= "Errore Giorni Cardio <br>";
+      }
+    }
+
+    if (!array_key_exists('Pesi', $_POST) && array_key_exists('Allenamento', $_POST) && $_POST["Allenamento"] == "si"){
+        $errori .= "Errore Pesi <br>";
+    }
+
+    if (array_key_exists('Pesi', $_POST) && array_key_exists('Allenamento', $_POST) && $_POST["Pesi"] == "si" && $_POST["Allenamento"] == "si"){
+      if(!is_numeric($_POST["GiorniPesi"]) || $_POST["GiorniPesi"] < 0 || $_POST["GiorniPesi"] > 7){
+        $errori .= "Errore Giorni Pesi <br>";
+      }
+    }
+    if($_POST["lavoro"] == "default"){
+      $errori .= "Errore lavoro <br>";
+    }
+	}
+
+
+$html = "<!DOCTYPE html>
+  <html lang='en' dir='ltr'>
+
+  <head>
+    <meta charset='utf-8'>
+    <title>ProjectWork</title>
+    <script src='../js/BMR.js' charset='utf-8'></script>
+    <link rel='stylesheet' href='../css/indexStyle.css'>
+  </head>
+
+  <body>
+      <div id='errori'>
+        $errori
+      </div>
+      <form method='POST' action='tdee.php'>
+        Formula:
+        <select id='formula' name='formule' onchange='controlloFormula(this.value);'>
+          <option value='default' selected hidden>Seleziona</option>
+          <option value='hb'>Harris-Benedict</option>
+          <option value='rhb'>Harris-Benedict Revisionata</option>
+          <option value='msj'>Mifflin-St Jeor</option>
+          <option value='km'>Katch-McArdle</option>
+          <option value='s'>Schofield</option>
+        </select><br>
+
+        <div class='campi'>
+          <span>Sesso:<input type='radio' id='maschio' name='Sesso' value='m' disabled='disabled'> Maschio <input type='radio' id='femmina' name='Sesso' value='f' disabled='disabled'> Femmina<br></span>
+          <span>Peso: <input type='text' name='Peso' id='Peso' disabled='disabled'><br></span>
+          <span>Altezza: <input type='text' name='Altezza' id='Altezza' disabled='disabled'><br></span>
+          <span>Età: <input type='text' name='Eta' id='Età' disabled='disabled'><br></span>
+          <span>Massa Grassa: <input type='text' name='MassaGrassa' id='MassaGrassa' disabled='disabled'><br></span>
+          <span>Allenamento:<input type='radio' id='si' name='Allenamento' value='si' disabled='disabled' onchange='controlloAllenamento()'> Si <input type='radio' id='no' name='Allenamento' value='no' disabled='disabled' onchange='controlloAllenamento()'> No<br></span>
+          <span>Cardio:<input type='radio' id='si' name='Cardio' value='si' disabled='disabled' onchange='controlloCardio()'> Si <input type='radio' id='no' name='Cardio' value='no' disabled='disabled' onchange='controlloCardio()'> No<br></span>
+          <span>N° Giorni: <input type='text' name='GiorniCardio' id='GiorniCardio' disabled='disabled'><br> </span>
+          <span>Pesi:<input type='radio' id='si' name='Pesi' value='si' disabled='disabled' onchange='controlloPesi()'> Si <input type='radio' id='no' name='Pesi' value='no' disabled='disabled' onchange='controlloPesi()'> No<br></span>
+          <span>N° Giorni: <input type='text' name='GiorniPesi' id='GiorniPesi' disabled='disabled'><br> </span>
+          Stile di vita: <select id='lavoro' name='lavoro' disabled='disabled'>
+            <option value='default' selected hidden>Seleziona</option>
+            <option value='1'>Sedentario</option>
+            <option value='2'>Leggermente attivo</option>
+            <option value='3'>Moderatamente attivo</option>
+            <option value='4'>Estremamente attivo</option>
+          </select><br>
+          <input type='hidden' id='prova' name='prova'>
+          <input type='submit' name='invia' onclick='bmr()' disabled='disabled'>
+      </form>
+    </div>
+
+  </body>
+  </html>
+";
+
+print($html);
+
+
+ ?>
