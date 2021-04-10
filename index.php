f06057ee984283c18dc874aacd79f72a40de2587
Alessandro Franceschini <alessandro.franceschini.2002@gmail.com>
2021-04-10 07:34:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<title>HTML Tutorial</title>
-<body>
-
-<h1>This is a heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html>
+<?php
+  header('Location: risultato.php');
+ ?>
