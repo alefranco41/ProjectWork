e714fd641de1c2edbb98af96a972bc0c6df27b3d
Alessandro Franceschini <alessandro.franceschini.2002@gmail.com>
2021-04-12 20:16:31 +0200

    Create utente1.sql

---
+++ PROJECTWORK/utente1.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 12, 2021 alle 10:05
+-- Versione del server: 10.1.36-MariaDB
+-- Versione PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_branceschini`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `password_hashed` varchar(100) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cognome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `TDEE` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `utente`
+--
+
+INSERT INTO `utente` (`username`, `password`, `password_hashed`, `nome`, `cognome`, `email`, `TDEE`) VALUES
+('alefranco41', 'dk5pQnhxRTR1TG02OUdSWmhGWFk0QT09', '$2y$10$7V7P9sRm589EsUnR5GtaGeyMVZIHrZzPUUtRJRpwYQXzj6HcOdz32', 'alessandro', 'franceschini', 'alessandro.franceschini.2002@gmail.com', 2861);
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
